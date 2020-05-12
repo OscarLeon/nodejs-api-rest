6d8d28594b6256ff0018b6864742ce913a17a8d5
Oscar <oscar.leon@4agile.cl>
2020-05-12 18:43:31 -0400

    FEAT: add configuration port and script start

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
 // Importamos las librerias para uso en el codigo
-var express = require('express');
-var http = require('http');
-var cors = require('cors');
-var app = express()
+var express = require("express");
+var http = require("http");
+var cors = require("cors");
+var app = express();
 
 // Habiliatamos para que pueda emitir respuesta desde cualquier origen
 app.use(cors());
+app.set("port", process.env.PORT || 9990);
 
 // Peticion GET para la informacion de los personajes de StarWars
-app.get('/api/characters/:character', (req, res) => {
+app.get("/api/characters/:character", (req, res) => {
   let characterObjectJson = null;
   let characterName = req.params.character;
-  let pathCharacter = './api/characters/';
-  let tagJson = '.json';
-  let joinUrlCharacter = '';
+  let pathCharacter = "./api/characters/";
+  let tagJson = ".json";
+  let joinUrlCharacter = "";
   switch (characterName) {
-    case 'luke':
-    case 'darth-vader':
+    case "luke":
+    case "darth-vader":
       joinUrlCharacter += `${pathCharacter}`;
       joinUrlCharacter += `${characterName}`;
       joinUrlCharacter += `${tagJson}`;
-      characterObjectJson = require(joinUrlCharacter)
+      characterObjectJson = require(joinUrlCharacter);
       res.status(200).send(characterObjectJson);
       break;
     default:
       res.status(404).send("No se encuentra informacion del personaje");
       break;
   }
-})
+});
 
 // Mensaje de Bienvenida en Ruta Raiz
-app.get('/', (req, res) => {
-  res.status(200).send("Welcome to API REST for BCI Front End")
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to API REST for BCI Front End");
+});
 
 // Levantamos el servidor
-http.createServer(app).listen(9990, () => {
-  console.log('Server started at http://localhost:9990');
+http.createServer(app).listen(app.get("port"), () => {
+  console.log("Server started at http://localhost:" + app.get("port"));
 });
